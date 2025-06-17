90cc64d5685be82892485158a8a066eb06f3dbed
Khanh <khanh@starchdev.com>
2025-06-17 16:59:50 -0500

    refactor: Separate flashcard array into separate file to import
    Signed-off-by: Khanh <khanh@starchdev.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import Flashcard from "./components/Flashcard";
-
-const flashcards = [
-    {
-        frontText:
-            "Several users of a shared printer complain that the printer is printing on both sides of a sheet of paper when it should only print on one side.\nWhat setting does a technician change?\n\nA. Orientation\nB. Badging\nC. Quality\nD. Duplex",
-        backText: "D. Duplex",
-    },
-    {
-        frontText:
-            "The projector in the conference room is experiencing intermittent shutdowns. The technician begins troubleshooting by checking all the connector cables.\nWhat are the next troubleshooting steps for the technician? (Select the best two choices.)\n\nA. Check that the vents are free from dust.\nB. Check that the bulb is not burned out.\nC. Check that the LED array is situated properly.\nD. Check that the fan is working.",
-        backText:
-            "A. Check that the vents are free from dust.\nD. Check that the fan is working.",
-    },
-    {
-        frontText:
-            "A universal asynchronous receiver transmitter (UART) port was traditionally a physical serial port in a legacy system.\nIn a modern mobile device, how can a UART be utilized? (Select all that apply.)\n\nA. Wi-Fi\nB. Bluetooth\nC.USB\nD.Ethernet",
-        backText: "B.Bluetooth\nC.USB",
-    },
-];
+import { flashcards } from "./data/flashcards";
 
 const App = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
---
+++ src/data/flashcards.js
@@ -0,0 +1,48 @@
+export const flashcards = [
+    {
+        frontText:
+            "Several users of a shared printer complain that the printer is printing on both sides of a sheet of paper when it should only print on one side.\nWhat setting does a technician change?\n\nA. Orientation\nB. Badging\nC. Quality\nD. Duplex",
+        backText: "D. Duplex",
+    },
+    {
+        frontText:
+            "The projector in the conference room is experiencing intermittent shutdowns. The technician begins troubleshooting by checking all the connector cables.\nWhat are the next troubleshooting steps for the technician? (Select the best two choices.)\n\nA. Check that the vents are free from dust.\nB. Check that the bulb is not burned out.\nC. Check that the LED array is situated properly.\nD. Check that the fan is working.",
+        backText:
+            "A. Check that the vents are free from dust.\nD. Check that the fan is working.",
+    },
+    {
+        frontText:
+            "A universal asynchronous receiver transmitter (UART) port was traditionally a physical serial port in a legacy system.\nIn a modern mobile device, how can a UART be utilized? (Select all that apply.)\n\nA. Wi-Fi\nB. Bluetooth\nC.USB\nD.Ethernet",
+        backText: "B.Bluetooth\nC.USB",
+    },
+    {
+        frontText:
+            "Which current USB standard supports 2 x 10 Gbps speeds and uses a USB-C connector type?\n\nA. USB 3.2 Gen1\nB. USB 3.2 Gen 2x1\nC. USB 3.2 Gen 2x2\nD. USB 3.1",
+        backText: "C.USB 3.2 Gen 2x2",
+    },
+    {
+        frontText:
+            "A computer technician replaces the power supply in a desktop system.  To do this, the technician needs to know the output of the current power supply unit (PSU).\nWhat is the PSU output capability measured as?\n\nA. Voltage\nB. Power\nC.Wattage rating\nD. Efficiency",
+        backText: "C.Wattage rating",
+    },
+    {
+        frontText:
+            "Systems administrators deploy a virtualization solution that separates host resources for each instance that is running at the operating system level.\nWhich solution is running?\n\nA. Type 1 hypervisor\nB. Type 2 hypervisor\nC. Container\nD. VM",
+        backText: "C.Container",
+    },
+    {
+        frontText:
+            "An organization experiences an internet outage. As fiber is being used, an engineer checks to see if the optical signal is being properly converted to an electrical signal. The router is connected using an RJ45 copper wire patch cord.\nWhat does the engineer check?\n\nA. OLT\nB. PoP\nC. PON\nD. ONT",
+        backText: "D.ONT",
+    },
+    {
+        frontText:
+            "A user states that a smartphone with a Li-ion battery does not hold a charge for as long as it did in the past and seems to charge slowly after replacing a lost adapter. The change was noticed over the last few months.\nWhat could be the cause? (Select all that apply.)\nA. Swelling\nB. Battery type\nC. Aging batter\nD. Aftermarket AC Adapter",
+        backText: "A.The imaging drum is not properly losing its charge.",
+    },
+    {
+        frontText:
+            "A printer technician troubleshoots a laser printer and determines that the exposing stage of the print job is the issue with print jobs printing incorrectly.    What does the technician suspect is happening?  A.The imaging drum is not properly losing its charge. B.The OS driver is improperly encoding the page in a page description language. C.The imaging drum is not being conditioned by the primary charge roller. D.The toner is not being fed evenly onto a magnetized developer roller.",
+        backText: "A.The imaging drum is not properly losing its charge.",
+    },
+];
