c36efea6443eb1c585375b6096d25431f9f10dd8
Khanh <khanh@starchdev.com>
2025-06-17 18:52:22 -0500

    add: Add more flashcards to JS array
    Signed-off-by: Khanh <khanh@starchdev.com>

--- src/data/flashcards.js
+++ src/data/flashcards.js
@@ -13,36 +13,91 @@ export const flashcards = [
     {
         frontText:
             "A universal asynchronous receiver transmitter (UART) port was traditionally a physical serial port in a legacy system.\nIn a modern mobile device, how can a UART be utilized? (Select all that apply.)\n\nA. Wi-Fi\nB. Bluetooth\nC.USB\nD.Ethernet",
-        backText: "B.Bluetooth\nC.USB",
+        backText: "B. Bluetooth\nC. USB",
     },
     {
         frontText:
             "Which current USB standard supports 2 x 10 Gbps speeds and uses a USB-C connector type?\n\nA. USB 3.2 Gen1\nB. USB 3.2 Gen 2x1\nC. USB 3.2 Gen 2x2\nD. USB 3.1",
-        backText: "C.USB 3.2 Gen 2x2",
+        backText: "C. USB 3.2 Gen 2x2",
     },
     {
         frontText:
             "A computer technician replaces the power supply in a desktop system.  To do this, the technician needs to know the output of the current power supply unit (PSU).\nWhat is the PSU output capability measured as?\n\nA. Voltage\nB. Power\nC.Wattage rating\nD. Efficiency",
-        backText: "C.Wattage rating",
+        backText: "C. Wattage rating",
     },
     {
         frontText:
             "Systems administrators deploy a virtualization solution that separates host resources for each instance that is running at the operating system level.\nWhich solution is running?\n\nA. Type 1 hypervisor\nB. Type 2 hypervisor\nC. Container\nD. VM",
-        backText: "C.Container",
+        backText: "C. Container",
     },
     {
         frontText:
             "An organization experiences an internet outage. As fiber is being used, an engineer checks to see if the optical signal is being properly converted to an electrical signal. The router is connected using an RJ45 copper wire patch cord.\nWhat does the engineer check?\n\nA. OLT\nB. PoP\nC. PON\nD. ONT",
-        backText: "D.ONT",
+        backText: "D. ONT",
     },
     {
         frontText:
-            "A user states that a smartphone with a Li-ion battery does not hold a charge for as long as it did in the past and seems to charge slowly after replacing a lost adapter. The change was noticed over the last few months.\nWhat could be the cause? (Select all that apply.)\nA. Swelling\nB. Battery type\nC. Aging batter\nD. Aftermarket AC Adapter",
-        backText: "A.The imaging drum is not properly losing its charge.",
+            "A printer technician troubleshoots a laser printer and determines that the exposing stage of the print job is the issue with print jobs printing incorrectly.\nWhat does the technician suspect is happening?\n\nA. The imaging drum is not properly losing its charge.\nB. The OS driver is improperly encoding the page in a page description language.\nC. The imaging drum is not being conditioned by the primary charge roller.\nD. The toner is not being fed evenly onto a magnetized developer roller.",
+        backText: "A. The imaging drum is not properly losing its charge.",
     },
     {
         frontText:
-            "A printer technician troubleshoots a laser printer and determines that the exposing stage of the print job is the issue with print jobs printing incorrectly.    What does the technician suspect is happening?  A.The imaging drum is not properly losing its charge. B.The OS driver is improperly encoding the page in a page description language. C.The imaging drum is not being conditioned by the primary charge roller. D.The toner is not being fed evenly onto a magnetized developer roller.",
-        backText: "A.The imaging drum is not properly losing its charge.",
+            "A user states that a smartphone with a Li-ion battery does not hold a charge for as long as it did in the past and seems to charge slowly after replacing a lost adapter. The change was noticed over the last few months.\nWhat could be the cause? (Select all that apply.)\n\nA. Swelling\nB. Battery type\nC. Aging battery\nD. Aftermarket AC Adapter",
+        backText: "C. Aging battery\nD.Aftermarket AC Adapter",
+    },
+    {
+        frontText:
+            "Which of the following solutions can mitigate debris reaching components and address occasional overheating in a system that's positioned safely away from other objects by facilitating airflow to cool down the internal components? (Select all that apply.)\n\nA. Add more RAM\nB. Check the motherboard\nC. Install a new system fan\nD. Use blanking plates",
+        backText: "C. Install a new system fan\nD. Use blanking plates",
+    },
+    {
+        frontText:
+            "A large technical firm deploys a cloud service. The service offers cloud storage along with web development capabilities. What deployment model does the firm utilize when a multi-tiered subscription option is available to the general consumers?\n\nA. Private\nB. Public\nC. Hybrid\nD. Community",
+        backText: "B. Public",
+    },
+    {
+        frontText:
+            "An engineer troubleshooting a network connection identifies a port flapping problem.\nWhat should the engineer investigate as a possible cause? (Select all that apply.)\n\nA. VLAN Assignment\nB. Ethernet cable\nC. NIC port\nD. Switch port",
+        backText: "B. Ethernet cable\nC. NIC port\nD. Switch port",
+    },
+    {
+        frontText:
+            "A user complains about the quality of the display on a laptop. The user mentions that the laptop has been in use since the company started many years ago. IT determines that the display uses a very early thin film transistor (TFT) technology and will replace the laptop.\nWhich TFT technology does the display use?\n\nA. Vertical alignment\nB. Organic LED\nC. In-plane switching\nD. Twisted nematic",
+        backText: "D. Twisted nematic",
+    },
+    {
+        frontText:
+            "An engineer configures 6 disks in a workstation to be part of a redundant array of independent disks (RAID) volume where one-sixth of the space on each disk will be used for parity. Which RAID type does the engineer implement?\n\nA. 5\nB. 0\nC. 1\nD. 10",
+        backText: "A. 5",
+    },
+    {
+        frontText:
+            "An engineer plans a fiber cable implementation between two campus buildings about 30 kilometers apart. Due to the distance between the locations, the engineer selects single-mode fiber.\nWhat specifications does the engineer need to consider with this decision?\n\nA. Large core, long distances, short wavelength\nB. Small core, long distances, long wavelength\nC. Large core, short distances, long wavelength\nD. Small core, short distances, short wavelength",
+        backText: "B. Small core, long distances, long wavelength",
+    },
+    {
+        frontText:
+            "A user complains that a computer is too slow when opening numerous applications. A technician suggests purchasing a new computer that features CMP capabilities to handle the user's workload.\nWhich statement describes the technician’s suggestion?\n\nA. A single execution unit and set of registers for processes\nB. A multi-socket motherboard for multiple CPUs\nC. Multiple processing cores combined in the same package\nD. Multiple parallel thread capability within a process",
+        backText: "C. Multiple processing cores combined in the same package",
+    },
+    {
+        frontText:
+            "Engineers need to share data between the team's laptop devices that are located in the same office space. The speed needs to be at least 50 Mbps.\nWhich technology do the engineers use?\n\nA. Wi-Fi\nB. Bluetooth\nC. CDMA\nD. NFC",
+        backText: "A. Wi-Fi",
+    },
+    {
+        frontText:
+            "A technician plugs a USB device into a computer and notices that the speeds are slightly less than expected.  The USB port and device are USB 3.x devices, and the achieved speed is roughly 9 Gbps.\nWhat USB standard is in use?\n\nA. USB 3.2 Gen 2x1\nB. USB 3.2 Gen 1\nC. USB 3.2 Gen 2x2\nD. USB 2.0",
+        backText: "A. USB 3.2 Gen 2x1",
+    },
+    {
+        frontText:
+            "An organization conducts a weekly video conference from its home office to its field locations. Recently, employees at the field locations have been complaining of poor audio and video quality.\nWhich protocol does an engineer monitor for performance issues?\n\nA. UDP\nB. TCP\nC. IP\nD. MAC",
+        backText: "A. UDP",
+    },
+    {
+        frontText:
+            "An engineer is evaluating a new unified threat management (UTM) appliance as a potential replacement for an outdated firewall.\nWhich feature related to website browsing is the engineer most likely to find in the UTM?\n\nA. Content filtering\nB. Data loss prevention\nC. Workflow automation\nD. Load balancing",
+        backText: "A. Content filtering",
     },
 ];
